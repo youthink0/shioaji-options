24067f1b9281f0909c50de9f36a63b949b0cc0be
youthink0 <a0985418957@gmail.com>
2022-11-28 21:54:08 +0800

    Get options's contract and order msg

--- tools/globals.py
+++ tools/globals.py
@@ -14,7 +14,7 @@ def initialize():
     global at_the_money_code, at_the_money 
     
     api = None
-    lastcontractprice={}
-    txo_weekly_dict={}
-    at_the_money_code = ""
-    at_the_money = 0
+    lastcontractprice={} #最新的各檔報價資訊
+    txo_weekly_dict={} #key為每檔之code， value則為該檔之call put資訊，
+    at_the_money_code = "" #價平code
+    at_the_money = 0 #價平和
